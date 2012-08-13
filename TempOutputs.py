e50b07b0d52df60d02de767b5310323e8d61fce3
John Morgan <jtmorgan61@gmail.com>
2012-08-13 15:35:31 -0700

    removed temp file used for tests

--- TempOutputs.py
+++
@@ -1,5 +0,0 @@
-## variables used to spin up demo data set prior to refactoring
-
-path="C:\Documents and Settings\Administrator\Desktop\NewGolgiTest\\"+filenumber+".txt"
-book.save("C:\Documents and Settings\Administrator\Desktop\NewGolgiTest\eachtreedendritetest.xls")
-
